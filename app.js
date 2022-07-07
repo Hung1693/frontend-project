3b2958943f703be4d411ccfd2b093d985ca4c145
HungNguyen16 <HungNguyen16@github.com>
2022-07-07 16:00:41 -0500

    add serch by author's name and id, back to top button

--- app.js
+++ app.js
@@ -21,111 +21,130 @@ function createCard(element) {
     $cardHeader = $('<div class="card-header bg-transparent border-success"></div>');
     $card.append($cardHeader);
 
-    $h2 = $(`<h2 class="card-title">${element.author}</h2>`);
+    $h2 = $(`<h2 class="card-title">${
+        element.author
+    }</h2>`);
     $cardHeader.append($h2);
 
     $cardBody = $('<div class="card-body"></div>');
     $card.append($cardBody);
 
-    $img = $(`<img src="${element.download_url}" class="card-img-top img-fluid rounded" alt="">`);
+    $img = $(`<img src="${
+        element.download_url
+    }" class="card-img-top img-fluid rounded" alt="">`);
     $cardBody.append($img);
 
     $cardFooter = $('<div class="card-footer bg-transparent border-success d-flex justify-content-between"></div>');
     $card.append($cardFooter);
 
-    $h3 = $(`<h3>ID: ${element.id}</h3>`);
+    $h3 = $(`<h3>ID: ${
+        element.id
+    }</h3>`);
     $thumbsUp = $(`<i class="bi bi-hand-thumbs-up-fill"></i>`)
     $cardFooter.append($h3);
     $cardFooter.append($thumbsUp);
-    
-    //FEATURE1: USERS CAN CLICK TO LIKE AND UNLIKE A IMAGE
+
+    // FEATURE1: USERS CAN CLICK TO LIKE AND UNLIKE A IMAGE
     $thumbsUp.click(function () {
         $(this).toggleClass('checked');
-        $imgModal = $(`<img class="img-fluid" src=${element.download_url} alt="">`)
-        if ($(this).hasClass('checked')) { 
+        $imgModal = $(`<img class="img-fluid" src=${
+            element.download_url
+        } alt="">`)
+        if ($(this).hasClass('checked')) {
             $(this).css('color', 'blue');
-            likedImg.push(element); 
+            likedImg.push(element);
         } else {
             $(this).css('color', 'gray');
-            likedImg.splice($.inArray(element, likedImg), 1);        
+            likedImg.splice($.inArray(element, likedImg), 1);
         }
     });
- 
+
 }
 
-//Use AJAX to call API
+// Use AJAX to call API
 $.get("https://picsum.photos/v2/list?limit=10", function (data) {
     data.map(element => {
         createCard(element);
     });
 });
 
-//FEATURE 2: USERS CAN SEE LIKED IMAGES 
+// FEATURE 2: USERS CAN SEE LIKED IMAGES
 let $likedImgNav = $('.nav-link');
 $likedImgNav.click(function () {
     $('.modal-body').empty();
     likedImg.forEach(element => {
-        let $imgModal = $(`<img class="img-fluid" src=${element.download_url} alt="">`)
+        let $imgModal = $(`<img class="img-fluid" src=${
+            element.download_url
+        } alt="">`)
         $('.modal-body').append($imgModal);
     });
     console.log(likedImg);
 });
 
 
-//FEATURE 3: USER SCROLL DOWN TO LOAD MORE IMAGES
+// FEATURE 3: USER SCROLL DOWN TO LOAD MORE IMAGES
 let flag = false;
 let count = 0;
-if (!flag) {
+if (! flag) {
     $(window).on("scroll", function () {
-    let scrollHeight = $(document).height();
-    let scrollPos = $(window).height() + $(window).scrollTop();
-    if(((scrollHeight - 300) >= scrollPos) / scrollHeight == 0) {
-        count++;
-        $.get(`https://picsum.photos/v2/list?page=${count+10}&limit=1`, function (data) {
-            data.forEach(element => {
-              createCard(element);
+        let scrollHeight = $(document).height();
+        let scrollPos = $(window).height() + $(window).scrollTop();
+        if (((scrollHeight - 300) >= scrollPos) / scrollHeight == 0) {
+            count++;
+            $.get(`https://picsum.photos/v2/list?page=${
+                count + 10
+            }&limit=1`, function (data) {
+                data.forEach(element => {
+                    createCard(element);
+                });
             });
-        });
-    }
-});
+        }
+    });
 }
 
 
-
-//FEATURE 4: DARK MODE SWITCH
+// FEATURE 4: DARK MODE SWITCH
 let $switch = $('#flexSwitchCheckDefault');
-$switch.click(function() {
+$switch.click(function () {
     $switch.toggleClass('dark');
     if ($switch.hasClass('dark')) {
         $('body').css('background-image', `url(https://images.unsplash.com/photo-1611416457332-946853cc75d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1571&q=80)`);
         $('body').css('color', `white`);
         $('.navbar').css('color', `white`);
         $('.nav-link').css('color', `white`);
-        
+
     } else {
         $('body').css('background-image', `url(https://images.unsplash.com/photo-1634655377962-e6e7b446e7e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80)`);
         $('body').css('color', `black`);
         $('.navbar').css('color', `black`);
         $('.nav-link').css('color', `black`);
-       
+
     }
 });
 
-//FEATURE 5: SEARCH IMG BY ID
-$('.search-button').click(() => {
-    // $('.modal-found').empty();
+// FEATURE 5: SEARCH IMG BY ID
+$('.search-button').click(() => { // $('.modal-found').empty();
     let $inputValue = $('.form-control').val();
-    let foundLink = `https://picsum.photos/id/${$inputValue}/500`;
-         let $imgFoundModal = $(`<img class="img-fluid" src=${foundLink} alt="">`)
+    console.log(typeof $inputValue);
+    if ($.isNumeric(parseInt($inputValue))) {
+        let foundLink = `https://picsum.photos/id/${$inputValue}/500`;
+        let $imgFoundModal = $(`<img class="img-fluid" src=${foundLink} alt="">`)
         $('.modal-found').append($imgFoundModal);
-    
-    console.log($inputValue);
+    }
+
+
+    $.get("https://picsum.photos/v2/list?limit=1000", function (data) {
+        data.map(element => {
+            if ($inputValue.toLowerCase() === element.author.toLowerCase()) {
+                let $imgFoundModal = $(`<img class="img-fluid" src=${
+                    element.download_url
+                } alt="">`)
+                $('.modal-found').append($imgFoundModal);
+                console.log(element);
+            }
+
+        });
+    });
+
+
 })
-
-
-
-
-
-
-
-
